e348acee48dc0b111f0331398838cad3c7315bf3
trahob <hoaibao.thb@gmail.com>
2024-06-05 11:57:27 +0700

    swinperBar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
 import topMenu from './components/topMenu.vue'
-
+import swiperBar from './components/swiperBar.vue'
+import SwiperCarousel from "./components/SwiperCarousel.vue";
 </script>
 
 <template>
     <topMenu/>
+    <swiperBar/>
+    <SwiperCarouselVue/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <SwiperCarousel />
+   
 </template>
 
 <style scoped>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/SwiperCaroucel.vue
@@ -0,0 +1,64 @@
+<template>
+    <swiper
+      :slides-per-view="3"
+      :space-between="50"
+      navigation
+      :pagination="{ clickable: true }"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide>
+        <h2>Slide 1</h2>
+        <p>--------</p>
+        <p>--------</p>
+        <p>--------</p>
+        <p>--------</p>
+      </swiper-slide>
+      <swiper-slide>
+        <h2>Slide 2</h2>
+      </swiper-slide>
+      <swiper-slide>
+        <h2>Slide 3</h2>
+      </swiper-slide>
+      <swiper-slide>
+        <h2>Slide 4</h2>
+      </swiper-slide>
+      <swiper-slide>
+        <h2>Slide 5</h2>
+      </swiper-slide>
+      <swiper-slide>
+        <h2>Slide 6</h2>
+      </swiper-slide>
+    </swiper>
+  </template>
+  <script>
+  // import Swiper core and required components
+  import SwiperCore, { Navigation, Pagination, A11y } from "swiper";
+  
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from "swiper/vue";
+  
+  // Import Swiper styles
+  import "swiper/swiper.scss";
+  import "swiper/components/navigation/navigation.scss";
+  import "swiper/components/pagination/pagination.scss";
+  
+  // install Swiper components
+  SwiperCore.use([Navigation, Pagination, A11y]);
+  
+  // Import Swiper styles
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    methods: {
+      onSwiper(swiper) {
+        // console.log(swiper)
+      },
+      onSlideChange() {
+        // console.log('slide change')
+      },
+    },
+  };
+  </script>
